0716b310fd2379dda8565367ef793e6292f59c52
Gabriel Dias <gabriel.diasramos@hotmail.com>
2023-09-14 22:08:28 -0300

    aula1 feita

---
+++ EstruturaDeDados/src/Aula1/App.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+
+        // Crie um array do tipo inteiro chamado nota com tamanho 5
+        int[] nota = new int[5];
+
+        // Crie um Scanner para ler as notas
+        Scanner leitor = new Scanner(System.in);
+
+        // Insira valores nesse array usando for
+        for (int i = 0; i < nota.length; i++) {
+            System.out.println("Digite a nota do aluno " + (i + 1));
+            nota[i] = leitor.nextInt();
+        }
+        // Imprima o array usando for
+        for (int i = 0; i < nota.length; i++) {
+            System.out.println("A nota do aluno " + (i + 1) + " é " + nota[i]);
+        }
+        //feche o scanner
+        leitor.close();
+
+        // System.out.println(nota.toString());
+        
+
+    }
+}
