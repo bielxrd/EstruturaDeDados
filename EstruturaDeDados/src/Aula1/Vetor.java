dbee16c38aa5898d6dac1db27e8ef7d38ec421d2
GABRIEL DIAS RAMOS <gabriel.dramos1@senacsp.edu.br>
2023-09-28 21:20:28 -0300

    adicionada funcoes(verdadeiro commit)

--- EstruturaDeDados/src/Aula1/Vetor.java
+++ EstruturaDeDados/src/Aula1/Vetor.java
@@ -10,7 +10,7 @@ public class Vetor {
         this.quantidade = 0;
     }
 
-    public boolean isEmpty() {
+    public boolean isEmpty() { // verifica se o vetor esta vazio
         if (quantidade == 0) {
             return true;
         } else {
@@ -19,7 +19,7 @@ public class Vetor {
 
     }
 
-    public boolean isFull() {
+    public boolean isFull() { // verifica se o vetor esta cheio
         if (quantidade == vetor.length) {
             return true;
         } else {
@@ -27,28 +27,34 @@ public class Vetor {
         }
     }
 
-    public void add(int valor) {
+    public boolean add(int valor) { // adiciona um elemento no vetor
         if (isFull()) {
             System.out.println("Vetor cheio");
+            return false;
         } else {
+            if(procurar(valor)) {
+                System.out.println("Valor ja existe no vetor");
+                return false;
+            }
             vetor[quantidade] = valor;
             quantidade++;
+            return true;
         }
     }
 
-    public boolean procurar(int valor) {
+    public boolean procurar(int valor) { // procura um elemento no vetor
         int i;
         for (i = 0; i < quantidade; i++)
-            if (vetor[i] == valor)
+            if (vetor[i] == valor) // encontrou o elemento
                 break;
 
-        if (i == quantidade)
+        if (i == quantidade) // nao encontrou o elemento e retorna falso(nao tem o elemento)
             return false;
         else
-            return true;
+            return true; // encontrou o elemento e retorna verdadeiro(tem o elemento)
     }
 
-    public void display() {
+    public void display() { // imprime o conteudo do vetor
         for (int i = 0; i < quantidade; i++) {
             System.out.print(vetor[i] + " ");
 
@@ -56,19 +62,55 @@ public class Vetor {
         }
     }
 
-    public boolean remover(int valor) {
+    public boolean remover(int valor) { // remove um elemento do vetor
         int i;
         for (i = 0; i < quantidade; i++) {
-            if (valor == vetor[i])
+            if (valor == vetor[i]) // encontrou o elemento
                 break;
         }
-        if (i == quantidade) {
+        if (i == quantidade) { // nao encontrou o elemento e retorna falso(nao tem o elemento)
             return false;
         } else {
-            for (int j = i; j < quantidade - 1; j++) {
-                vetor[j] = vetor[j + 1];
+            for (int j = i; j < quantidade; j++) {
+                if (j == vetor.length - 1) // se for o ultimo elemento, coloca 0
+                    vetor[j] = 0;
+                else // se nao for o ultimo elemento, coloca o proximo elemento no lugar
+                    vetor[j] = vetor[j + 1];
             }
             quantidade--;
+            return true;
+        }
+    }
+
+    public int getPosicao(int valor) {
+        int i;
+        for (i = 0; i < quantidade; i++) {
+            if (valor == vetor[i]) // encontrou o elemento
+                break;
+        }
+        if (i == quantidade)
+            return -1;
+        else
+            return i;
+
+    }
+    public boolean alterar(int atual, int novo) {
+        if(isEmpty()) {
+            System.out.println("Vetor vazio");
+            return false;
+        }
+        int i;
+        for ( i = 0; i < quantidade; i++) { // procura o elemento
+            if(atual==vetor[i]) { // encontrou o elemento
+                break; 
+            }
+        }
+        if(i == quantidade) { // nao encontrou o elemento e retorna falso(nao tem o elemento)
+            System.out.println("Valor nao existe no vetor");
+            return false;
+        }
+        else { 
+            vetor[i] = novo; 
             return true;
         }
     }
