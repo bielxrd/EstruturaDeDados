7f936f0a5ee9496b9e4063320795eb698a500bd2
GABRIEL DIAS RAMOS <gabriel.dramos1@senacsp.edu.br>
2023-10-20 20:18:33 -0300

    Alteracao da Main da Pilha Dinamica para aceitar tipo Generico

--- EstruturaDeDados/src/PilhaDinamica/Main.java
+++ EstruturaDeDados/src/PilhaDinamica/Main.java
@@ -2,11 +2,11 @@ package PilhaDinamica;
 
 public class Main {
     public static void main(String[] args) {
-        PilhaDinamica pilha = new PilhaDinamica();
+        PilhaDinamica<Character> pilha = new PilhaDinamica();
 
-        pilha.push(10);
-        pilha.push(20);
-        pilha.push(30);
+        pilha.push('g');
+        pilha.push('a');
+        pilha.push('b');
 
         System.out.println("Pilha:");
         pilha.display();
