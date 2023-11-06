24fbfdab9ddf10aeae27f3b1efe4eadd3b5c6d46
Gabriel Dias <129539000+bielxrd@users.noreply.github.com>
2023-11-06 02:10:31 -0300

    Classa cliente

---
+++ ADO2/AdoFila/src/Exercicio/Cliente.java
@@ -0,0 +1,41 @@
+package Exercicio;
+
+public class Cliente {
+    private String nome;
+    private boolean vip;
+    private int mesa;
+
+    public Cliente(String nome, boolean isVip) {
+        this.nome = nome;
+        this.vip = isVip;
+    }
+
+    public Cliente(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public boolean isVip() {
+        return vip;
+    }
+
+    public void setVip(boolean vip) {
+        this.vip = vip;
+    }
+
+    public int getMesa() {
+        return mesa;
+    }
+
+    public void setMesa(int mesa) {
+        this.mesa = mesa;
+    }
+
+}
