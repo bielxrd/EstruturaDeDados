e290735734c6982d19d47ba116562bcf7aef63bb
Gabriel Dias <129539000+bielxrd@users.noreply.github.com>
2023-11-10 01:33:41 -0300

    uso do LocalDateTime na classe cliente

--- ADO2/AdoFila/src/Exercicio/Cliente.java
+++ ADO2/AdoFila/src/Exercicio/Cliente.java
@@ -1,23 +1,31 @@
+
 package Exercicio;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
 
 public class Cliente {
     private String nome;
     private boolean vip;
     private int mesa;
+    private LocalDateTime date;
 
     public Cliente(String nome, boolean isVip) {
         this.nome = nome;
         this.vip = isVip;
+        this.date = date.now();
     }
 
     public Cliente(String nome, boolean vip, int mesa) {
         this.nome = nome;
         this.vip = vip;
         this.mesa = mesa;
+        this.date = date.now();
     }
 
     public Cliente(String nome) {
         this.nome = nome;
+        this.date = date.now();
     }
 
     public String getNome() {
@@ -42,6 +50,14 @@ public class Cliente {
 
     public void setMesa(int mesa) {
         this.mesa = mesa;
+    }
+
+    public LocalDateTime getDate() {
+        return date;
+    }
+
+    public void setChegada(LocalDateTime chegada) {
+        this.date = chegada;
     }
 
 }
