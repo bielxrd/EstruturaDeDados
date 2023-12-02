5cd64f520bc4dbbf0cd34cb4ff7f435c86f05e78
Gabriel Dias <129539000+bielxrd@users.noreply.github.com>
2023-12-02 03:34:52 -0300

    Classe Elemento responsavel pelo valor

---
+++ EstruturaDeDados/src/ArvoreBinaria/Elemento.java
@@ -0,0 +1,18 @@
+package ArvoreBinaria;
+
+public class Elemento {
+    private int valor;
+
+
+    public Elemento(int valor) {
+        this.valor = valor;
+    }
+
+    public int getValor() {
+        return valor;
+    }
+
+    public void setValor(int valor) {
+        this.valor = valor;
+    }
+}
