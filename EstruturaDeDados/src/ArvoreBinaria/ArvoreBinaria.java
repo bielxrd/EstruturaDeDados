12f1f3582b7076cf9becb1aaad173aaa3551e74b
Gabriel Dias <129539000+bielxrd@users.noreply.github.com>
2023-12-07 22:35:48 -0300

    Arvore Binaria (metodo remocao para todos os casos)

--- EstruturaDeDados/src/ArvoreBinaria/ArvoreBinaria.java
+++ EstruturaDeDados/src/ArvoreBinaria/ArvoreBinaria.java
@@ -18,6 +18,50 @@ public class ArvoreBinaria {
         this.esq = null;
     }
 
+    public ArvoreBinaria remover(Elemento elem) {
+        // Achei o elemento
+        if (this.elemento.getValor() == elem.getValor()) {
+            // Caso simples - Elemento esta em um no folha (Nao tem filhos)
+            if (this.esq == null && this.dir == null) {
+                return null;
+            } else {
+                // caso 2 - caso eu tenho filhos a esquerda, porem nao tenho a direita
+                if (this.esq != null && this.dir == null) {
+                    return this.esq;
+
+                    // caso 3 - caso eu tenho filhos a direita, porem nao tenho a esquerda
+                } else if (this.dir != null && this.esq == null) {
+                    return this.dir;
+                }
+                // caso 4 - tenho filhos dos dois lados
+                else {
+                    //Estrategia do menor dentro os maiores
+                    ArvoreBinaria aux = this.esq;
+
+                    while(aux.dir != null) {
+                        aux = aux.dir;
+                    }
+
+                    //Troco os elementos da arvore
+                    this.elemento = aux.getElemento(); // o nó atual recebe o elemento do aux (o maior dentro dos os menores)
+                    aux.setElemento(elem); // Insiro no nó folha (la embaixo do maior entre os menores) o elemento a ser removido
+
+                    this.esq = esq.remover(elem);
+
+                }
+
+            }
+
+        } else if (this.elemento.getValor() > elem.getValor()) {
+            //Delegar a responsabilidade para a sub arvore da esquerda
+            this.esq = this.esq.remover(elem);
+        } else if (this.elemento.getValor() < elem.getValor()) {
+            // Delegar a responsabilidade para a sub arvore da direita
+            this.dir = this.dir.remover(elem);
+        }
+        return this;
+    }
+
     public boolean isEmpty() {
         return this.elemento == null;
     }
@@ -35,8 +79,8 @@ public class ArvoreBinaria {
     }
 
     public void imprimirInOrdem() {
-        if(!isEmpty()) {
-            if(this.esq != null) {
+        if (!isEmpty()) {
+            if (this.esq != null) {
                 this.esq.imprimirInOrdem();
             }
             System.out.print(this.elemento.getValor() + "  ");
@@ -48,11 +92,11 @@ public class ArvoreBinaria {
     }
 
     public void imprimirPosOrdem() {
-        if(!isEmpty()) {
-            if(this.esq != null) {
+        if (!isEmpty()) {
+            if (this.esq != null) {
                 this.esq.imprimirPosOrdem();
             }
-            if(this.dir != null) {
+            if (this.dir != null) {
                 this.dir.imprimirPosOrdem();
             }
             System.out.print(this.elemento.getValor() + "  ");
