023a25bbe2b8102910a0cc62d5784827e7262bfa
Gabriel Dias <129539000+bielxrd@users.noreply.github.com>
2023-12-07 22:28:58 -0300

    Implementando arvore binaria e remover

--- EstruturaDeDados/src/ArvoreBinaria/AppArvore.java
+++ EstruturaDeDados/src/ArvoreBinaria/AppArvore.java
@@ -2,19 +2,22 @@ package ArvoreBinaria;
 
 public class AppArvore {
     public static void main(String[] args) {
-        ArvoreBinaria arvoreBinaria = new ArvoreBinaria(new Elemento(10));
+        ArvoreBinaria arvoreBinaria = new ArvoreBinaria(new Elemento(8));
 
         arvoreBinaria.inserir(new Elemento(5));
         arvoreBinaria.inserir(new Elemento(1));
-        arvoreBinaria.inserir(new Elemento(8));
+        arvoreBinaria.inserir(new Elemento(7));
         arvoreBinaria.inserir(new Elemento(15));
         arvoreBinaria.inserir(new Elemento(12));
         arvoreBinaria.inserir(new Elemento(18));
 
-        arvoreBinaria.imprimirPreOrdem();
-        System.out.println();
         arvoreBinaria.imprimirInOrdem();
         System.out.println();
-        arvoreBinaria.imprimirPosOrdem();
+        Elemento elem = new Elemento(18);
+        arvoreBinaria = arvoreBinaria.remover(elem);
+
+        System.out.println("Nova arvora: ");
+        arvoreBinaria.imprimirInOrdem();
+        System.out.println();
     }
 }
