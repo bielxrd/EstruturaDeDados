5a96634ed048a4770faf85530d39a3c2daa477f6
Gabriel Dias <129539000+bielxrd@users.noreply.github.com>
2023-12-12 02:04:09 -0300

    Mudanca nos metodos inserir e remover para verificar o balanceamento, metodos rotacaoSimplesDir, SimplesEsq, DuplaDir, DuplaEsq, metodos veificar balanceamento, calcularaltura e calcularbalanceamento

--- EstruturaDeDados/src/ArvoreAVL/ArvoreAVL.java
+++ EstruturaDeDados/src/ArvoreAVL/ArvoreAVL.java
@@ -35,49 +35,172 @@ public class ArvoreAVL {
 
     }
 
+    public void calcularBalanceamento() {
+        if (this.dir == null && this.esq == null) {
+            this.bal = 0;
+        } else if (this.esq == null && this.dir != null) {
+            this.bal = this.dir.calcularAltura() - 0;
+        } else if (this.esq != null && this.dir == null) {
+            this.bal = 0 - this.esq.calcularAltura();
+        } else {
+            this.bal = this.dir.calcularAltura() - this.esq.calcularAltura();
+        }
+
+        if (this.dir != null) this.dir.calcularBalanceamento();
+        if (this.esq != null) this.esq.calcularBalanceamento();
+
+
+    }
+
+
+    public ArvoreAVL verificarBalanceamento() {
+
+        if (this.bal >= 2 || this.bal <= -2) {
+            if (this.bal >= 2) {
+                if (this.bal * this.dir.getBalanceamento() > 0) {
+                    System.out.println("ROTACAO SIMPLES A DIREITA");
+                    return rotacaoSimplesDireita();
+                } else {
+                    System.out.println("ROTACAO DUPLA A DIREITA");
+                    return rotacaoDuplaDireita();
+                }
+            } else {
+                if (this.bal * this.esq.getBalanceamento() > 0) {
+                    System.out.println("ROTACAO SIMPLES A ESQUERDA");
+                    return rotacaoSimplesEsquerda();
+                } else {
+                    System.out.println("ROTACAO DUPLA A ESQUERDA");
+                    return rotacaoDuplaEsquerda();
+                }
+            }
+        }
+        this.calcularBalanceamento();
+        if (this.esq != null) this.esq = this.esq.verificarBalanceamento();
+        if (this.dir != null) this.dir = this.dir.verificarBalanceamento();
+        return this;
+    }
+
+    public ArvoreAVL rotacaoSimplesDireita() {
+        ArvoreAVL filhoDir;
+        ArvoreAVL filhoDoFilho = null;
+
+        filhoDir = this.getDireita();
+        if (this.dir != null) {
+            if (this.dir.getEsquerda() != null) {
+                filhoDoFilho = filhoDir.getEsquerda();
+            }
+        }
+        filhoDir.setEsquerda(this);
+        this.setDireita(filhoDoFilho);
+
+        return filhoDir;
+
+    }
+
+    public ArvoreAVL rotacaoSimplesEsquerda() {
+        ArvoreAVL filhoEsq;
+        ArvoreAVL filhoDoFilho = null;
+
+        filhoEsq = this.getEsquerda();
+        if (this.esq != null) {
+            if (this.esq.getDireita() != null) {
+                filhoDoFilho = filhoEsq.getDireita();
+            }
+        }
+        filhoEsq.setDireita(this);
+        this.setEsquerda(filhoDoFilho);
+
+        return filhoEsq;
+    }
+
+    public ArvoreAVL rotacaoDuplaDireita() {
+        ArvoreAVL arvore = this;
+        ArvoreAVL filhoDir = this.getDireita();
+        ArvoreAVL filhoDoFilho = filhoDir.getEsquerda();
+        ArvoreAVL noInserido = filhoDoFilho.getDireita();
+
+        // Primeiro passo - alinhar
+
+        filhoDir.setEsquerda(noInserido);
+        filhoDoFilho.setDireita(filhoDir);
+        this.setDireita(filhoDoFilho);
+
+        // parte 2
+
+        ArvoreAVL novoFilhoDireita = this.getDireita();
+        arvore.setDireita(null);
+        novoFilhoDireita.setEsquerda(arvore);
+
+        return novoFilhoDireita;
+
+
+    }
+
+    public ArvoreAVL rotacaoDuplaEsquerda() {
+        ArvoreAVL arvore = this;
+        ArvoreAVL filhoEsq = this.getEsquerda();
+        ArvoreAVL filhoDoFilho = filhoEsq.getDireita();
+        ArvoreAVL noInserido = filhoDoFilho.getEsquerda();
+
+        // Primeiro passo - alinhar
+
+        filhoEsq.setDireita(noInserido);
+        filhoDoFilho.setEsquerda(filhoEsq);
+        this.setEsquerda(filhoDoFilho);
+
+        // parte 2
+
+        ArvoreAVL novoFilhoEsquerda = this.getEsquerda();
+        arvore.setEsquerda(null);
+        novoFilhoEsquerda.setDireita(arvore);
+
+        return novoFilhoEsquerda;
+    }
+
     public ArvoreAVL remover(Elemento elem) {
         // Achei o elemento
         if (this.elemento.getValor() == elem.getValor()) {
-            // Caso simples - Elemento esta em um no folha (Nao tem filhos)
+            // Caso simples - Elemento está em um nó folha (não tem filhos)
             if (this.esq == null && this.dir == null) {
                 return null;
             } else {
-                // caso 2 - caso eu tenho filhos a esquerda, porem nao tenho a direita
+                // Caso 2 - Tenho filhos à esquerda, mas não tenho à direita
                 if (this.esq != null && this.dir == null) {
                     return this.esq;
 
-                    // caso 3 - caso eu tenho filhos a direita, porem nao tenho a esquerda
+                    // Caso 3 - Tenho filhos à direita, mas não tenho à esquerda
                 } else if (this.dir != null && this.esq == null) {
                     return this.dir;
                 }
-                // caso 4 - tenho filhos dos dois lados
+                // Caso 4 - Tenho filhos dos dois lados
                 else {
-                    //Estrategia do menor dentro os maiores
+                    // Estratégia do menor entre os maiores
                     ArvoreAVL aux = this.esq;
 
                     while (aux.dir != null) {
                         aux = aux.dir;
                     }
 
-                    //Troco os elementos da arvore
-                    this.elemento = aux.getElemento(); // o nó atual recebe o elemento do aux (o maior dentro dos os menores)
-                    aux.setElemento(elem); // Insiro no nó folha (la embaixo do maior entre os menores) o elemento a ser removido
+                    // Troco os elementos da árvore
+                    this.elemento = aux.getElemento(); // o nó atual recebe o elemento do aux (o maior entre os menores)
+                    aux.setElemento(elem); // Insiro no nó folha (lá embaixo do maior entre os menores) o elemento a ser removido
 
                     this.esq = esq.remover(elem);
-
                 }
-
             }
-
         } else if (this.elemento.getValor() > elem.getValor()) {
-            //Delegar a responsabilidade para a sub arvore da esquerda
+            // Delegar a responsabilidade para a subárvore da esquerda
             this.esq = this.esq.remover(elem);
         } else if (this.elemento.getValor() < elem.getValor()) {
-            // Delegar a responsabilidade para a sub arvore da direita
+            // Delegar a responsabilidade para a subárvore da direita
             this.dir = this.dir.remover(elem);
         }
-        return this;
+
+        // Após a remoção, calcular o balanceamento e verificar se ajustes são necessários
+        this.calcularBalanceamento();
+        return this.verificarBalanceamento();
     }
+
 
     public boolean isEmpty() {
         return this.elemento == null;
@@ -154,18 +277,24 @@ public class ArvoreAVL {
                 if (this.esq == null) { // Nó folha
                     this.esq = novaArvore;
                 } else {
-                    this.esq = this.esq.inserir(novo); //Insere na esuqerda caso ja tenha uma raiz da esquerda (verifica recursivamnete o esq dentro da arvore esq que é null e adiciona)
+                    this.esq = this.esq.inserir(novo); // Insere na esquerda e atualiza a referência
                 }
             } else if (novo.getValor() > this.elemento.getValor()) { // Inserir na direita
                 if (this.dir == null) {
                     this.dir = novaArvore;
                 } else {
-                    this.dir = this.dir.inserir(novo); //Insere na direita caso ja tenha uma raiz da direita (verifica recursivamnete o esq dentro da arvore esq que é null e adiciona)
+                    this.dir = this.dir.inserir(novo); // Insere na direita e atualiza a referência
                 }
             }
+
+            // Após a inserção, calcular o balanceamento e verificar se ajustes são necessários
+            this.calcularBalanceamento();
+            return this.verificarBalanceamento();
         }
+
         return this;
     }
+
 
     public String printArvore(int level) {
         String str = this.toString() + "\n";
@@ -186,7 +315,7 @@ public class ArvoreAVL {
         }
         if (this.dir != null) {
 
-            str = "+-DIR: " + this.dir.printArvore(level + 1);
+            str += "+-DIR: " + this.dir.printArvore(level + 1);
 
         } else {
             str += "+-DIR: NULL";
@@ -197,7 +326,7 @@ public class ArvoreAVL {
     }
 
     public String toString() {
-        return "[" + this.getElemento().getValor() + "]";
+        return "[" + this.getElemento().getValor() + "] (" + this.bal + ")";
     }
 
 
