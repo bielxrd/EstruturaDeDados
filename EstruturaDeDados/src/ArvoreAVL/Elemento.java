7d6f2bab558fdc9e612d479a86e7ba8cf6c0c535
Gabriel Dias <129539000+bielxrd@users.noreply.github.com>
2023-12-12 00:30:45 -0300

    classe elemento

---
+++ EstruturaDeDados/src/ArvoreAVL/Elemento.java
@@ -0,0 +1,14 @@
+package ArvoreAVL;
+
+public class Elemento {
+    private int valor;
+    public Elemento(int valor) {
+        this.valor = valor;
+    }
+    public int getValor() {
+        return valor;
+    }
+    public void setValor(int valor) {
+        this.valor = valor;
+    }
+}
