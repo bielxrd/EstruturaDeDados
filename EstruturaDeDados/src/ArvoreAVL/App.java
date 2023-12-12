db057dcd3d4ef3f4c429eda162f7303d953cf79b
Gabriel Dias <129539000+bielxrd@users.noreply.github.com>
2023-12-12 02:02:56 -0300

    Testando a arvore AVL COMPLETA

--- EstruturaDeDados/src/ArvoreAVL/App.java
+++ EstruturaDeDados/src/ArvoreAVL/App.java
@@ -6,13 +6,26 @@ public class App {
     public static void main(String[] args) {
 
         Scanner ler = new Scanner(System.in);
-        
+
+        System.out.print("Digite o valor da raiz da árvore AVL: ");
         ArvoreAVL arvoreAVL = new ArvoreAVL(new Elemento(ler.nextInt()));
+
         System.out.println(arvoreAVL.printArvore(0));
 
         while (true) {
-            arvoreAVL = arvoreAVL.inserir(new Elemento(ler.nextInt()));
+            System.out.print("Digite o valor a ser inserido na árvore (ou digite 'sair' para encerrar): ");
+            String input = ler.next();
+
+            if (input.equalsIgnoreCase("sair")) {
+                System.out.println("Encerrando o programa.");
+                break;
+            }
+
+            int valor = Integer.parseInt(input);
+            arvoreAVL = arvoreAVL.inserir(new Elemento(valor));
             System.out.println(arvoreAVL.printArvore(0));
         }
+
+        ler.close();
     }
 }
