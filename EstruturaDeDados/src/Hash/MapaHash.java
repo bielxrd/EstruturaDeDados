92aaa2ac0c1faea984953cdccd01b7c33874b69c
Gabriel Dias <129539000+bielxrd@users.noreply.github.com>
2023-12-13 19:02:27 -0300

    Tratando as colisoes do primeiro registro, dos registros do meio, do ultimo registro e usando conceito de lista encadeada e tendo sempre um proximo (lastnode) e ajuste no == para equals

--- EstruturaDeDados/src/Hash/MapaHash.java
+++ EstruturaDeDados/src/Hash/MapaHash.java
@@ -7,31 +7,62 @@ public class MapaHash<K, V> {
         valores = (Registro<K, V>[]) new Registro<?, ?>[100];
     }
 
-    public void put(Registro<K, V> registro) {
+    public void put(Registro registro) {
         int posicao = registro.hashCode();
-
         if (valores[posicao] == null) {
             valores[posicao] = registro;
+            System.out.println("Posicao vazia!! Novo Registro");
         } else {
+            System.out.print("Ops… colisao");
             Registro reg = valores[posicao];
 
-            while(reg.getProximo() != null) {
-                if (reg.getKey() == registro.getKey()) { // Registro novo igual a um existente ?
-                    reg.setValue(registro.getValue()); // Substituo o valor
-                } else {
-                    reg = reg.getProximo();
-                }
+            // especial para tratar o primeiro elemento
+            if (reg.getKey().equals(registro.getKey())) { // O registro que quero inserir é o mesmo que ja existe?
+                reg.setValue(registro.getValue()); // Substituo
+                System.out.println("É o primeiro Registro Existente – atualizando");
+                return;
             }
-            reg.setProximo(registro); // Coloquei o novo registro na ultima posicao
+
+            // se ele tá no meio da lista
+            while (reg.getProximo() != null) {
+                if (reg.getKey().equals(registro.getKey())) { // O registro que quero inserir é o mesmo que ja existe?
+                    reg.setValue(registro.getValue()); // Substituo
+                    System.out.println("Tá no meio da lista – Registro Existente – atualizando…");
+                    return;
+                }
+                reg = reg.getProximo();
+            }
+
+            // se ele é o último da lista
+            if (reg.getKey().equals(registro.getKey())) {
+                reg.setValue(registro.getValue());
+                System.out.println("É o ultimo! Registro Existente – atualizando…");
+                return;
+            }
+
+            reg.setProximo(registro); // COloquei o nvoo registro na ultima posicao
+            System.out.println("Novo Registro!");
         }
     }
+
 
     public Registro<K, V> get(K key) {
         Registro<K, V> r = new Registro();
         try {
             r.setKey(key);
             int posicao = r.hashCode();
-            return valores[posicao];
+            Registro resultado = valores[posicao]; //Tento encontrar o registro
+            if (resultado != null && resultado.getKey().equals(key)) { // é este o registro?
+                return resultado; //s
+            } else {
+                while (resultado != null) { //enquanto tiver na lista
+                    resultado = resultado.getProximo(); // vou pro proximo
+                    if (resultado != null && resultado.getKey().equals(key)) { // é o cara? return
+                        return resultado;
+                    }
+                }
+            }
+            return null;
         } catch (ArrayIndexOutOfBoundsException e) {
             System.out.println(e.getMessage());
             return null;
