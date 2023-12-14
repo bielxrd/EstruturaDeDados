ea13b9bf85392cf3f58cc72112d35f5425b9f416
Gabriel Dias <129539000+bielxrd@users.noreply.github.com>
2023-12-14 16:27:39 -0300

    Metodo BubbleSort e Testando

---
+++ EstruturaDeDados/src/BubbleSort/AlgoritmosDeOrdenacao.java
@@ -0,0 +1,33 @@
+package BubbleSort;
+
+public class AlgoritmosDeOrdenacao {
+    private int[] vetor = {25, 57, 48, 37, 12, 92, 33};
+
+    public void bubbleSort() {
+        int i;
+        int pos;
+        int temp;
+
+        for (i = 0; i < vetor.length - 1; i++) {
+            for (pos = 0; pos < vetor.length - i - 1; pos++) {
+                // Comparo o elemento da posicao com seu proximo
+                if (vetor[pos] > vetor[pos + 1]) {
+                    // Entao troca
+                    temp = vetor[pos];
+                    vetor[pos] = vetor[pos + 1];
+                    vetor[pos + 1] = temp;
+                }
+            }
+        }
+        System.out.println("Final da iteracao: "+i);
+        mostraVetor();
+    }
+
+    public void mostraVetor() {
+        for (int i = 0; i < vetor.length; i++) {
+            System.out.print(vetor[i] + " ");
+        }
+        System.out.println("");
+    }
+
+}
---
+++ EstruturaDeDados/src/BubbleSort/MinhaApp.java
@@ -0,0 +1,8 @@
+package BubbleSort;
+
+public class MinhaApp {
+    public static void main(String[] args) {
+        AlgoritmosDeOrdenacao alg = new AlgoritmosDeOrdenacao();
+        alg.bubbleSort();
+    }
+}
