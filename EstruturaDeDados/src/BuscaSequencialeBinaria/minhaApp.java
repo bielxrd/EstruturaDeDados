4db3b06b7f0bb4dc36bf284f56129de5cf6d1e8f
Gabriel Dias <129539000+bielxrd@users.noreply.github.com>
2023-12-14 14:11:46 -0300

    TEstando busca binaria

--- EstruturaDeDados/src/BuscaSequencialeBinaria/minhaApp.java
+++ EstruturaDeDados/src/BuscaSequencialeBinaria/minhaApp.java
@@ -1,16 +1,23 @@
 package BuscaSequencialeBinaria;
 
+import java.sql.SQLOutput;
+
 public class minhaApp {
     public static void main(String[] args) {
 
         Colecao colecao = new Colecao();
 
+
+        System.out.println("------------------------- BUSCA SEQUENCIAL -------------------------");
         System.out.println("Posicao do elemento 23: "+colecao.buscaSequencial(23));
         System.out.println("Posicao do elemento 12: "+colecao.buscaSequencial(12));
         System.out.println("Posicao do elemento 76: "+colecao.buscaSequencial(76));
         System.out.println("Posicao do elemento 100: "+colecao.buscaSequencial(100));
 
 
-
+        System.out.println("------------------------- BUSCA BINARIA -------------------------");
+        System.out.println("Posicao do elemento 12: "+colecao.buscaBinaria(12));
+        System.out.println("Posicao do elemento 23: "+colecao.buscaBinaria(23));
+        System.out.println("Posicao do elemento 87: "+colecao.buscaBinaria(87));
     }
 }
