6254c3ab0ac2787708dd5fa2cf53af52ee01273b
Gabriel Dias <129539000+bielxrd@users.noreply.github.com>
2023-12-14 14:12:01 -0300

    Metodo ordenar, mostrar array e busca binaria

--- EstruturaDeDados/src/BuscaSequencialeBinaria/Colecao.java
+++ EstruturaDeDados/src/BuscaSequencialeBinaria/Colecao.java
@@ -14,4 +14,46 @@ public class Colecao {
         return -1;
     }
 
+    public int buscaBinaria(int valor) {
+        this.valores = ordenar();
+        int meio, inicio, fim;
+        inicio = 0;
+        fim = valores.length - 1;
+
+        while (inicio <= fim) {
+            meio = (inicio + fim) / 2;
+            if (valores[meio] == valor) {
+                return meio;
+            } else {
+                if (valor > valores[meio]) {
+                    inicio = meio + 1;
+                } else {
+                    fim = meio - 1;
+                }
+            }
+        }
+        return -1;
+
+    }
+
+    public int[] ordenar() {
+        for (int i = 0; i < valores.length; i++) {
+            for (int j = 0; j < valores.length - i - 1; j++) {
+                if (valores[j] > valores[j + 1]) {
+                    int aux = valores[j];
+                    valores[j] = valores[j + 1];
+                    valores[j + 1] = aux;
+                }
+            }
+        }
+        return this.valores;
+    }
+
+    public void mostrarArray() {
+        for (int valores: valores) {
+            System.out.print(valores + " - ");
+        }
+    }
+
+
 }
