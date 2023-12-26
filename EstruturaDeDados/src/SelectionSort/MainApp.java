05f6a5e352a06ebbb377596f5231325c86ac94a0
Gabriel Dias <129539000+bielxrd@users.noreply.github.com>
2023-12-26 17:59:46 -0300

    Testando Selection Sort

---
+++ EstruturaDeDados/src/SelectionSort/MainApp.java
@@ -0,0 +1,13 @@
+package SelectionSort;
+
+public class MainApp {
+    public static void main(String[] args) {
+
+
+        SelectionSort alg = new SelectionSort();
+
+        alg.mostraVetor();
+        alg.selectionSort();
+
+    }
+}
